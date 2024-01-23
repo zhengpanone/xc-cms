29a4e176899a1d12a35751d29e718e11cf9a3e39
zhengpanone <zhengpanone@htomail.com>
2024-01-23 16:42:12 +0800

    页面静态化

--- xc-model/src/main/java/com/zp/model/cms/CmsConfigModel.java
+++ xc-model/src/main/java/com/zp/model/cms/CmsConfigModel.java
@@ -18,10 +18,12 @@ import java.util.Map;
 public class CmsConfigModel {
     @Id
     private String key;// 主键
-    @Schema(description = "项目名称",defaultValue = "项目名称", title = "id")
+    @Schema(description = "项目名称", defaultValue = "项目名称", title = "id")
     private String name; // 项目名称
-    @Schema(description = "项目url",defaultValue = "http://www.example.com", title = "url")
+    @Schema(description = "项目url", defaultValue = "http://www.example.com", title = "url")
     private String url; // 项目url
+    @Schema(description = "项目复杂值")
     private Map mapValue; // 项目复杂值
+    @Schema(description = "项目简单值")
     private String value; // 项目简单值
 }
